a8a6b3d97df7869f710bfae3e34a2c32406c0207
RAJ KUMAR MALLEBOINA <sushmithaengr23@gmail.com>
2025-03-05 14:27:49 +0530

    .

---
+++ OneDrive/Desktop/vite-react/my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+const Home=()=>{
+  return (
+<div style={{ textAlign: "center", padding: "20px" }}>
+  <h1>Sushmitha Malleboina</h1>
+  <p>9632581478</p>
+</div>
+);
+};
+
+function App() {
+  return <Home />;
+}
+export default App;
