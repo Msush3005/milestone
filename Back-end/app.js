25cacc6a30f8e9d4bd4944b9ae0bf34575036f16
RAJ KUMAR MALLEBOINA <sushmithaengr23@gmail.com>
2025-04-07 22:55:40 +0530

    done

--- Back-end/app.js
+++ Back-end/app.js
@@ -4,16 +4,18 @@ const app = express();
 app.use(express.json());
 const ErrorMiddleware= require("./middleware/error")
 const path=require("path")
+const cookieParser =require("cookie-parser")
+app.use(cookieParser())
 
 app.use(cors({
-  origin:"*",
+  origin:"http://localhost:5173",
   credentials:true
 }))
 
 const {userRoute} = require('./controllers/userRoute');
 
 const productRouter = require("./controllers/productRoutes");
-
+const orderRouter =require("./controllers/orderRoute")
 
 
 app.get("/test", async (req, res) => {
@@ -24,12 +26,13 @@ app.get("/test", async (req, res) => {
 console.log(path.join(__dirname, 'uploadproducts'))
 
 
-app.use('/profile-photo', express.static(path.join(__dirname, 'uploads')));
+app.use('/profile-photo', express.static(path.join(__dirname, 'upload')));
 
 app.use('/products-photo', express.static(path.join(__dirname, 'uploadproducts')));
 
 app.use("/user",userRoute)
 app.use("/product", productRouter);
+app.use("/order",orderRouter)
 
 
 
